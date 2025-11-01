f6cee3c012e52375bb99b8e82057f1e95a775c99
Brocolitx <grivyzom@gmail.com>
2025-11-01 16:51:20 -0300

    feat: Se añade el componente Footer al App.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './core/components/navbar/navbar';
 import { NotificationBar } from './core/components/notification-bar/notification-bar';
+import { Footer } from './core/components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, NotificationBar],
+  imports: [RouterOutlet, Navbar, NotificationBar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
